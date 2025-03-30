8b5ed6c57a001e4f63bdc637ae9ac8093411e14b
Danilo-266] <lucashenriquedejesus98@gmail.com>
2025-03-30 19:16:01 -0300

    Update score display colors for improved visibility and consistency

--- lib/jogo.dart
+++ lib/jogo.dart
@@ -148,15 +148,15 @@ class _JogoState extends State<Jogo> {
               children: [
                 Text(
                   'Jogador: $pontuacao',
-                  style: TextStyle(fontSize: tamanhojogador, color: tamanhojogador == 25 ? Color.fromARGB(255, 151, 0, 252) : Color.fromARGB(255, 54, 73, 63), fontFamily: "BubblegumSans"), 
+                  style: TextStyle(fontSize: tamanhojogador, color: tamanhojogador == 25 ? Color.fromRGBO(155, 93, 229, 1) : Color.fromARGB(255, 54, 73, 63), fontFamily: "BubblegumSans"), 
                 ),
                 Text(
                   'Empate: $pontuacaoEmpate',
-                  style: TextStyle(fontSize: tamanhoempate, color: tamanhoempate == 25 ?  Color.fromARGB(255, 151, 0, 252) : Color.fromARGB(255, 54, 73, 63), fontFamily: "BubblegumSans"),
+                  style: TextStyle(fontSize: tamanhoempate, color: tamanhoempate == 25 ?  Color.fromRGBO(155, 93, 229, 1) : Color.fromARGB(255, 54, 73, 63), fontFamily: "BubblegumSans"),
                 ),
                 Text(
                   'Computador: $pontuacaoComputador',
-                  style: TextStyle(fontSize: tamanhocomputador, color: tamanhocomputador == 25 ?  Color.fromARGB(255, 151, 0, 252) : Color.fromARGB(255, 54, 73, 63), fontFamily: "BubblegumSans"),
+                  style: TextStyle(fontSize: tamanhocomputador, color: tamanhocomputador == 25 ?  Color.fromRGBO(155, 93, 229, 1) : Color.fromRGBO(54, 73, 63, 1), fontFamily: "BubblegumSans"),
                 ),
               ],
             ),
